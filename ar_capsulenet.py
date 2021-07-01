4b2711f428e6f1a7fdcb960cdfb0cad4c38316f8
Rajbir Bhattacharjee <rajbir.bhattacharjee@gmail.com>
2021-07-01 20:37:39 +0100

    asdfasdf
    asdf

--- ar_capsulenet.py
+++ ar_capsulenet.py
@@ -238,8 +238,8 @@ def train(train_model, x_train, y_train, args):
 
 def load_mnist():
     # the data, shuffled and split between train and test sets
-    from keras.datasets import mnist
-    from keras.utils import to_categorical
+    from tensorflow.keras.datasets import mnist
+    from tensorflow.keras.utils import to_categorical
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
     x_train = x_train.reshape(-1, 28, 28, 1).astype('float32') / 255.
